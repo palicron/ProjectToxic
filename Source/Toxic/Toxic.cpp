e318b3539a4d00b81bcaa33e288461d35319b8ea
Palicron <elgranpalicron@gmail.com>
2023-03-25 13:16:56 -0500

    Init Push

---
+++ Source/Toxic/Toxic.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Toxic.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Toxic, "Toxic" );
+
+DEFINE_LOG_CATEGORY(LogToxic)
+ 
---
+++ Source/Toxic/ToxicGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToxicGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AToxicGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AToxicGameMode();
+};
+
+
+
